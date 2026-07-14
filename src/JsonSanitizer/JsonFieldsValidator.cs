f250cbf4ffc9a109340c4727d2df2b2d5ef74706
david stern <dzs101010@gmail.com>
2026-07-14 16:21:15 +0300

    Field validations

---
+++ src/JsonSanitizer/JsonFieldsValidator.cs
@@ -0,0 +1,107 @@
+using System;
+namespace DroneFleetDataProcessing.JsonFieldsValidator;
+interface IFieldsValidator //Normal Validity Check
+{
+    void RegularValidator(string field);
+}
+interface IFieldsUniqueValidator//Field Uniqueness Check
+{
+    void UniqueValidator(string field, List<string> strings);
+}
+interface IFieldsCertainRangeValidator//Range Validity Check
+{
+    void ertainRangeValidator(string field, double min, double max);
+}
+public class CustomeException : Exception//Custom error class
+{
+    public CustomeException(string message) : base(message)
+    {
+    }
+}
+class isIntAndPositive: IFieldsValidator
+{
+    public void RegularValidator(string field)
+    {
+        if(!int.TryParse(field, out var value))
+        {
+            throw new CustomeException($"The value is not an integer.");
+        }
+        else if(value  < 0)
+        {
+            throw new CustomeException($"The number is not an positive.");
+        }
+    }
+}
+class isUnique: IFieldsUniqueValidator
+{
+    public void UniqueValidator(string field, List<string> strings)
+    {
+        if(strings.Contains(field))
+        {
+            throw new CustomeException($"The value is not unique.");
+        }
+    }
+}
+class EmptyOrSpaces: IFieldsValidator
+{
+    public void RegularValidator(string field)
+    {
+        if(field.Length == 0 || string.IsNullOrEmpty(field))
+        {
+            throw new CustomeException($"The value is empty.");
+        }
+    }
+}
+class serialNumberFormat: IFieldsValidator
+{
+    public void RegularValidator(string field)
+    {
+        string[] parts = field.Split("-");
+        if(parts[0] != "DR")
+        {
+            throw new CustomeException($"The value is Does not start with DR.");
+        }
+        if (!int.TryParse(parts[1], out var value) || parts[1].Length < 4)
+        {
+            throw new CustomeException($"The value is not in the required format.");
+        }
+    }
+}
+class isAertainWord : IFieldsUniqueValidator
+{
+    public void UniqueValidator(string field, List<string> strings)
+    {
+        if (!strings.Contains(field))
+        {
+            throw new CustomeException($"The value is not in the specific values.");
+        }
+    }
+}
+class isertainRange: IFieldsCertainRangeValidator
+{
+    public void ertainRangeValidator(string field, double min, double max)
+    {
+        if(!double.TryParse(field, out var value))
+        {
+            throw new CustomeException($"The value is not an decimal number.");
+        }
+        if (double.Parse(field) <= min)
+        {
+            throw new CustomeException($"The number is too small.");
+        }
+        else if(double.Parse(field) >= max)
+        {
+            throw new CustomeException($"The number is too large.");
+        }
+    }
+}
+class smallBatteryHealth
+{
+    public void ValidsmallBatteryHealth(string batteryHealth, string status)
+    {
+        if(!double.TryParse(batteryHealth, out double value) && value < 20 && status == "Operational")
+        {
+            throw new CustomeException($"The status cannot be Operational because the battery health is less than 20.");
+        }
+    }
+}
