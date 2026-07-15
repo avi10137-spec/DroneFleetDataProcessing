06371f1bc027b6388971e78df786c5afe226cc2b
david stern <dzs101010@gmail.com>
2026-07-15 10:38:13 +0300

    update utils

--- src/utils.cs
+++ src/utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace DroneFleetDataProcessing.src
 {
@@ -8,11 +9,34 @@ namespace DroneFleetDataProcessing.src
     {
         public string GetPath(params string[] pathSegment)
         {
-          string baseDir = AppContext.BaseDirectory;
+           string baseDir = AppContext.BaseDirectory;
            string[] allPaths = new[] { baseDir }.Concat(pathSegment).ToArray();
            string filePath = Path.Combine(allPaths);
            Console.WriteLine($"path is {filePath}");
-            return filePath;
+           return filePath;
         }
     }
 }
+interface IWriter
+{
+    void writeToFile(string path, List<string> data);
+}
+class ToFile : IWriter
+{
+    public void writeToFile(string path, List<string> data)
+    {
+        string directoryPath = Path.GetDirectoryName(path);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        using (StreamWriter writer = new StreamWriter(path, append: true))
+        {
+            foreach (var line in data)
+            {
+                writer.WriteLine(line);
+            }
+        }
+    }
+}
+
