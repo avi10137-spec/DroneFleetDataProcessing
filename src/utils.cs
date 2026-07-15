311ba3383d46ae415097dab798b02fe3f22ee1b2
avi <avi10137@gmail.com>
2026-07-15 09:59:06 +0300

    add utils

---
+++ src/utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DroneFleetDataProcessing.src
+{
+    class FindPath
+    {
+        public string GetPath(params string[] pathSegment)
+        {
+          string baseDir = AppContext.BaseDirectory;
+           string[] allPaths = new[] { baseDir }.Concat(pathSegment).ToArray();
+           string filePath = Path.Combine(allPaths);
+           Console.WriteLine($"path is {filePath}");
+            return filePath;
+        }
+    }
+}
