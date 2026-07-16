742f57b5f9d108fd4f586598f0b2998afacccdcc
david stern <dzs101010@gmail.com>
2026-07-16 14:59:56 +0300

    write to terminal3

--- src/utils.cs
+++ src/utils.cs
@@ -14,7 +14,7 @@ interface IStringWriter
 }
 interface IOutputWriter
 {
-    void writeToFile(string stringData);
+    void write(string stringData);
 }
 class ToFile : IWriter
 {
@@ -65,7 +65,7 @@ class ToFileWithAppend :IStringWriter
 }
 class ToTheTerminal: IOutputWriter //write to terminal
 {
-    public void writeToFile(string stringData)
+    public void write(string stringData)
     {
         Console.WriteLine(stringData);
     }
